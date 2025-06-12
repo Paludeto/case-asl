fa12f88f2cbc1ef2799adc2b5cf08bb778232818
Paludeto <gpalu54@gmail.com>
2025-06-12 01:01:55 -0300

    refatorada

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ Uso:
 
 from pathlib import Path
 from typing import List, Tuple
-from collections import Counter
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,57 +16,54 @@ from wfdb import processing
 # Classe na qual armazenamos informações do sinal
 class ECGEntry:
 
-    """
-    Representa um único registro MIT-BIH (apenas 1 canal).
-
-    Atributos públicos
-    ------------------
-    signal_mv : np.ndarray
-        Amplitudes do ECG em milivolts.
-    sampling_rate_hz : float
-        Frequência de amostragem.
-    sample_indices : np.ndarray
-        Índices inteiros de 0 … N-1.
-    r_peaks_indices : np.ndarray
-        Posições estimadas dos picos R (QRS) pelo algoritmo XQRS.
-    annotated_r_peaks : List[Tuple[int, str]]
-        Pares (índice, símbolo) onde o R-peak coincide com a anotação `atr`.
-    """
+    _record:            wfdb.Record
+    _ann:               wfdb.Annotation
+    _signal_mv:         np.ndarray
+    _sample_indices:    np.ndarray
+    _r_peaks_indices:   np.ndarray
+    _annotated_r_peaks: List[Tuple[int, str]]
 
     # Construtor
-    def __init__(self, idx: int, db_dir: str | Path = "db", channel: int = 0):
-        
+    def __init__(self, idx: int):
+
+        self._load_data(idx)
+        self._detect_peaks()
+
+        # Combina instantes dos picos R com anotações => CONSERTAR ISSO, FAZER DE FORMA MAIS CLEAN!!!
+        lookup = {s: sym for s, sym in zip(self._ann.sample, self._ann.symbol)}
+        self._annotated_r_peaks: List[Tuple[int, str]] = [
+            (i, lookup[i]) for i in self._r_peaks_indices if i in lookup
+        ]
+
+    def _load_data(self, idx: int, db_dir: str = 'db') -> None:
+
         path = Path(__file__).resolve().parent.parent / db_dir / f"{idx}"
 
         try:
-            self.record = wfdb.rdrecord(str(path), channels=[channel])
-            self.ann = wfdb.rdann(str(path), extension="atr")
+            self._record = wfdb.rdrecord(str(path), channels=[0])
+            self._ann = wfdb.rdann(str(path), extension="atr")
         except FileNotFoundError as exc:
             raise FileNotFoundError(f"Registro {idx} não encontrado em {path.parent}") from exc
 
         # Extração de atributos do record
-        self.signal_mv: np.ndarray = self.record.p_signal[:, 0]
-        self.sample_indices: np.ndarray = np.arange(self.record.sig_len)
+        self._signal_mv: np.ndarray = self._record.p_signal[:, 0]
+        self._sample_indices: np.ndarray = np.arange(self._record.sig_len)
+    
+    def _detect_peaks(self) -> None:
 
         # Detecção de picos R em sinais QRS
-        det = processing.XQRS(self.signal_mv, fs=self.record.fs)
+        det = processing.XQRS(self._signal_mv, fs=self._record.fs)
         det.detect()
-        self.r_peaks_indices: np.ndarray = det.qrs_inds
-
-        # Combina instantes dos picos R com anotações
-        lookup = {s: sym for s, sym in zip(self.ann.sample, self.ann.symbol)}
-        self.annotated_r_peaks: List[Tuple[int, str]] = [
-            (i, lookup[i]) for i in self.r_peaks_indices if i in lookup
-        ]
+        self._r_peaks_indices: np.ndarray = det.qrs_inds
 
     # Funções de plot, usando janela de 100 segundos
     def plot(self, beat: int, window: int = 50) -> None:
-        
-        idx = self.annotated_r_peaks[beat][0]
+
+        idx = self._annotated_r_peaks[beat][0]
         
         sl = slice(idx - window, idx + window)
 
-        plt.plot(list(range(0,100)), self.signal_mv[sl])
+        plt.plot(list(range(0,100)), self._signal_mv[sl])
         plt.xlabel("Amostras")
         plt.ylabel("mV")
 
@@ -95,9 +91,9 @@ if __name__ == "__main__":
 
     # print(entry4.ann.aux_note)
 
-    print(entry.annotated_r_peaks[1553])
-    print(entry2.annotated_r_peaks[1134])
-    print(entry3.annotated_r_peaks[1572])
+    print(entry._annotated_r_peaks[1553])
+    print(entry2._annotated_r_peaks[1134])
+    print(entry3._annotated_r_peaks[1572])
 
     entry.multi_plot(normal=0, atipico=1553, titulo=f'Batimento Normal x Contração Ventricular Prematura ({100})')  # ~25:13 PVCs
     entry2.multi_plot(normal=0, atipico=1134, titulo=f'Batimento Normal x Contração Ventricular Prematura ({201})') # ~24:15 Aberrated atrial couplet, fusion PVC
