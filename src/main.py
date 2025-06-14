031b1740f74f36a4397ffa762dd685a171df7184
gabsprog <zedi.gamers@gmail.com>
2025-06-14 16:59:13 -0300

    dicionario, media, plot

--- src/main.py
+++ src/main.py
@@ -62,13 +62,13 @@ class ECGEntry:
         ]
 
     # Funções de plot, usando janela de 100 segundos
-    def plot(self, beat: int, window: int = 50) -> None:
+    def plot(self, beat: int, window: int = 100) -> None:
 
         idx = self._annotated_r_peaks[beat][0]
         
         sl = slice(idx - window, idx + window)
 
-        plt.plot(list(range(0,100)), self._signal_mv[sl])
+        plt.plot(list(range(0,200)), self._signal_mv[sl])
         plt.xlabel("Amostras")
         plt.ylabel("mV")
 
@@ -84,6 +84,58 @@ class ECGEntry:
         plt.tight_layout()
         plt.show()
 
+    
+    # Cria um dicionário que mapeia cada tipo de batimento para uma lista de amostras em que esses batimentos ocorrem no sinal
+    def build_annotation_table(self) -> dict:
+        tabela = {}
+
+        for idx, tipo in self._annotated_r_peaks:
+            if tipo not in tabela:
+                tabela[tipo] = []
+            tabela[tipo].append(idx)
+
+        return tabela
+    
+    # Retorna uma lista com as posições na lista de batimentos anotados que correspondem ao tipo informado
+    def get_beats_by_annotation(self, tipo: str) -> List[int]:
+        return [i for i, (_, t) in enumerate(self._annotated_r_peaks) if t == tipo]
+    
+    # Calcula a forma de onda média para um tipo de batimento
+    def calculate_average_beat(self, beat_type: str, window_size: int = 100) -> np.ndarray | None:
+        tabela = self.build_annotation_table()
+        locations = tabela.get(beat_type, [])
+
+        all_beats_signals = []
+        for loc in locations:
+            start = loc - window_size
+            end = loc + window_size
+            if start >= 0 and end < len(self._signal_mv):
+                all_beats_signals.append(self._signal_mv[start:end])
+
+        if not all_beats_signals:
+            print(f"Não foi possível extrair janelas de sinal para o tipo '{beat_type}'.")
+            return None
+        
+        return np.mean(np.array(all_beats_signals), axis=0)
+    
+    # Plota a média das ondas no gráfio
+    def plot_waveforms(self, waveforms: dict, titulo: str):
+        plt.figure(figsize=(12, 7))
+        plt.title(titulo)
+
+        for label, waveform in waveforms.items():
+            if waveform is not None:
+                window_size = len(waveform) // 2
+                x_axis = np.arange(-window_size, window_size)
+                plt.plot(x_axis, waveform, label = f"{label} (média dos batimentos)")
+                plt.xlabel("Amostras (em relação ao pico R)")
+        plt.ylabel("mV")
+        plt.grid(True)
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+
+
 if __name__ == "__main__":
 
     entry = ECGEntry(100)
@@ -91,14 +143,36 @@ if __name__ == "__main__":
     entry3 = ECGEntry(105)
     entry4 = ECGEntry(200)
 
-    # print(entry4.ann.aux_note)
+    normais_100 = entry.get_beats_by_annotation('N')
+    ventriculares_100 = entry.get_beats_by_annotation('V')
 
-    print(entry._annotated_r_peaks[1553])
-    print(entry2._annotated_r_peaks[1134])
-    print(entry3._annotated_r_peaks[1572])
-    print(entry4._annotated_r_peaks[11])
+    normais_201 = entry2.get_beats_by_annotation('N')
+    ventriculares_201 = entry2.get_beats_by_annotation('V')
 
-    entry.multi_plot(normal=0, atipico=1553, titulo=f'Batimento Normal x Contração Ventricular Prematura ({100})')  #   ~25:13 PVCs
-    entry2.multi_plot(normal=0, atipico=1134, titulo=f'Batimento Normal x Contração Ventricular Prematura ({201})') #   ~24:15 Aberrated atrial couplet, fusion PVC
-    entry3.multi_plot(normal=1, atipico=1572, titulo=f'Batimento Normal x Contração Ventricular Prematura ({105})') #   26:45 PVC
-    entry4.multi_plot(normal=0, atipico=11, titulo=f'Batimento Normal x Contração Ventricular Prematura ({200})')   #   1:42 Ventricular tachycardia, 3 beats
+    normais_105 = entry3.get_beats_by_annotation('N')
+    ventriculares_105 = entry3.get_beats_by_annotation('V')
+
+    normais_200 = entry4.get_beats_by_annotation('N')
+    ventriculares_200 = entry4.get_beats_by_annotation('V')
+
+    entry.multi_plot(normal=normais_100[0], atipico=ventriculares_100[0], titulo=f'Batimento Normal x PVC ({100})')
+
+    entry2.multi_plot(normal=normais_201[0], atipico=ventriculares_201[0], titulo=f'Batimento Normal x PVC ({201})')
+
+    entry3.multi_plot(normal=normais_105[1], atipico=ventriculares_105[0], titulo=f'Batimento Normal x PVC ({105})')
+
+    entry4.multi_plot(normal=normais_200[0], atipico=ventriculares_200[0], titulo=f'Batimento Normal x PVC ({200})')
+
+
+    avg_normal_beat = entry2.calculate_average_beat(beat_type='N')
+    avg_pvc_beat = entry2.calculate_average_beat(beat_type='V')
+
+    formas_de_onda_para_plotar = {
+        "Normal": avg_normal_beat,
+        "PVC": avg_pvc_beat,
+    }
+
+    entry.plot_waveforms(
+        waveforms=formas_de_onda_para_plotar,
+        titulo=f'Comparação de Moldes Médios (Registro {entry2._record.record_name})'
+    )
